d44a6f72e27f0f9af31bb315c9777e00c827d9e9
qitian <qitia@192.168.2.16>
2020-09-14 17:22:10 -0400

    somehow working again

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// 200200914
+#include <iostream> 
+using namespace std;
+
+int main(){
+    cout << "igor best prof!";
+}
+
+
+
+// 20200910
+/*
+#include <iostream>
+
+int main(){
+    double x = 234.1;
+    double y = 34.6;
+    std::cout << "hi";
+}
+*/
--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(){
-    double x = 234.1;
-    double y = 34.6;
-    std::cout << "hi";
-}
