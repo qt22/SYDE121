274d8945c61c7be6b826d641949f380fc941b9ab
qitian <qitia@192.168.2.16>
2020-10-11 03:18:51 -0400

    a2_p1 done but need comment; a2_p2 started

--- a2_p1.cpp
+++ a2_p1.cpp
@@ -18,7 +18,7 @@
 
 // program steps:
 // 
-// Test cases
+// Test cases: 
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -27,21 +27,32 @@ using namespace std;
 int num_of_digits(long int n);
 // PURPOSE: This function computes the number of digits of the given integer.
 
+void break_down_digits(int digits[], int size, int n);
+// PURPOSE: This function assign digits of an integer into an array.
+// INPUT: An array, its size and the integer provided by user.
+// OUTPUT: N/A 
+
 bool odd_sum_of_digits(long int n);
 // PURPOSE: This function checks if the sum of the given integer is odd.
-// INPUT: n, the integer received from the user
+// INPUT: n, the integer received from the user.
 // OUTPUT: 1 if the sum of digits of n is odd, 0 if the sum of digits 
 //         of n is even.
 
 bool palindrome(long int n);
 // PURPOSE: This function checks if the given integer is palindromic.
-// INPUT: n, the integer received from the user
+// INPUT: n, the integer received from the user.
 // OUTPUT: 1 if the integer n is palindromic, 0 if the integer n 
 //         is not palindromic.
 
+bool prime(long int n);
+// PURPOSE: This function checks if the given integer is a prime number.
+// INPUT: n, the integer received from the user.
+// OUTPUT: 1 if the integer n is a prime number, 0 if the integer n 
+//         is not a prime number.
+
 void compute_prime_numbers();
 // PURPOSE: Find all the prime numbers from 1 to the positive integer 
-//          provided by user.
+//          provided by user and identify those with special cases.
 // INPUT: The upper bond of the prime number inputted via console.
 // OUTPUT: Palindromic prime numbers less than 10000, odd sum of digits  
 //         prime numbers less than 100000 and all other prime numbers  
@@ -57,29 +68,37 @@ int num_of_digits(long int n){
     return num_digits;
 }
 
-bool odd_sum_of_digits(long int n){
-    const int TEN = 10, EVEN = 2;
+void break_down_digits(int digits[], int size, long int n){
+    const int TEN = 10, NUM_DIGITS = num_of_digits(n);
     const double HALF = 0.5;
-    int num_digits = num_of_digits(n);
-    int copy_num_digits = num_digits, index = 0, sum = 0;
 
-    int digits[num_digits] = { 0 };
+    int copy_num_digits = NUM_DIGITS, index = 0;
 
-    
     while(n){
         copy_num_digits--;
-        int thousand = int(pow(TEN, copy_num_digits) + HALF);
-        digits[index] =  n / thousand;
-        n = n - digits[index] * thousand;
+        int tens = int(pow(TEN, copy_num_digits) + HALF);
+        digits[index] =  n / tens;
+        n = n - digits[index] * tens;
         index++;
     }
+}
 
-    for(int index = 0; index < num_digits; index++){
-        cout << digits[index] << '\n';
+bool odd_sum_of_digits(long int n){
+    const int NUM_DIGITS = num_of_digits(n),
+              DEFAULT_VALUE = 0;
+    int copy_num_digits = NUM_DIGITS, index = 0, sum = 0;
+
+    int digits[NUM_DIGITS] = { DEFAULT_VALUE };
+
+    break_down_digits(digits, NUM_DIGITS, n);
+
+    for(int index = 0; index < NUM_DIGITS; index++){
+        // cout << digits[index] << '\n';
         sum += digits[index];
     }
 
-    if(sum % EVEN == 0){
+    bool even = sum % 2 == 0;
+    if(even){
         return false;
     }else{
         return true;
@@ -87,12 +106,78 @@ bool odd_sum_of_digits(long int n){
 }
 
 bool palindrome(long int n){
-    int num_digits = num_of_digits(n);
-    if(num_digits % 2 == 0){
-        
+    const int NUM_DIGITS = num_of_digits(n), HALF = NUM_DIGITS/2,
+              DEFAULT_VALUE = 0;
+    int copy_of_num_digits = NUM_DIGITS;
+    int digits[NUM_DIGITS] = { DEFAULT_VALUE };
+
+    break_down_digits(digits, NUM_DIGITS, n);
+
+    for(int index = 0; index < HALF; index++){
+        if(digits[index] != digits[--copy_of_num_digits]){
+            return false;
+            break;
+        }
     }
+    
+    return true;
 
 }
+
+bool prime(long int n){
+    bool if_prime = false;
+    const int LEAST_PRIME_NUM = 2;
+
+    if(n == LEAST_PRIME_NUM){
+        return true;
+    }
+    for(int index = LEAST_PRIME_NUM; index < n; index++){
+        if(n % index == 0){
+            if_prime = false;
+            break;
+        }else{
+            if_prime = true;
+        }
+    }
+    return if_prime;
+}
+
+void compute_prime_numbers(){
+    long int n = 0;
+    const int LOWER_LIMIT = 1, START_NUM = 2, 
+              TEN_THOUSAND = 10000, HUNDRED_THOUSAND = 100000;
+    
+    while(true){
+        cout << "Please choose an integer that is greater than 1: " << endl;
+        double value = 0;
+        cin >> value;
+        cin.get();
+        if(value > LOWER_LIMIT){
+            n = int(value);
+            break;
+        }
+        cout << "Please read the instruction carefully!" << endl;
+    }
+
+    for(int index = START_NUM; index <= n; index++){
+        bool condition1 = prime(index);
+        if(condition1){
+            cout << index << " is a prime number. ";
+        }
+        bool condition2 = prime(index) && palindrome(index);
+        if(condition2 && index < TEN_THOUSAND){
+            cout << "It is also palindromic. ";
+        }
+        bool condition3 = prime(index) && odd_sum_of_digits(index);
+        if(condition3 && index < HUNDRED_THOUSAND){
+            cout << "Its sum of digits is odd as well.";
+        }
+        if(condition1 || condition2 || condition3){
+            cout << '\n';
+        }
+    }
+}
+
 int main(){
-    cout << "odd sum: " << odd_sum_of_digits(2314);
+    compute_prime_numbers();
 }
--- a2_p2.cpp
+++ a2_p2.cpp
@@ -1,23 +1,94 @@
-/*
-    your  name,  
-    student  number, 
-    date  the  project  was  written,
-    course,   
-    lab room,   
-    assignment number,   
-    exercise number, 
-    filename, 
-    the name you gave the C++ project.  
-*/
+//*****************************************************************************
+// Student Name: Jerry Tian
+// Student Number: 20894361
+//
+// SYDE 121 Assignment: Assignment #2
+// Filename: a2_p2.cpp
+//
+// I hereby declare that this code, submitted for credit for the course
+// SYDE121, is a product of my own efforts. This coded solution has
+// not been plagiarized from other sources and has not been knowingly
+// plagiarized by others.
+//
+// Project: Distance of the baseball
+// Purpose: #1: Find the distance of the baseball has fallen in each 1-second 
+//          interval for the first 30 seconds along with the total distance 
+//          traveled at the end of each interval.
+//          #2: Find the amount of time that takes the baseball to reach the
+//          ground when it is released at 15000 meters.
+// Due Date: Friday, October 9, 2020
+//**************************************************************
 
-/*
-    File purpose
-*/
-// program steps
-// Test cases
+// Program steps:
+// Step 1: Create a function that computes the distance using the formula
+//         with given start time and end time.
+// Step 2: Create another function that calculate the total distance 
+//         with end time.
+// Step 3: Create another function that has two arrays. One for the distance
+//         during interval, the other for the total distance. Get both using 
+//         the functions above. Then output them in a for loop with proper 
+//         formatting.
+// Step 4: Create another function that finds the time before the total 
+//         distance reaches 15000 meters. Then find the exact time when the 
+//         the baseball touches 15000 meters and round it up to 1 decimal place.
+//
+// Test cases: 
 #include <iostream>
 using namespace std;
 
-int main() {
+double distance(int start_time, int end_time);
+// PURPOSE: Find the distance that the baseball traveled due to gravity
+//          within the given time.
+// INPUT: start_time, the time when the baseball is dropped;
+//        end_time, the ending time of the time interval.
+// OUTPUT: The distance traveled during the time interval. 
 
+double distance(double start_time, double end_time);
+// PURPOSE: Find the distance that the baseball traveled due to gravity
+//          within the given time.
+// INPUT: start_time, the time when the baseball is dropped;
+//        end_time, the ending time of the time interval.
+// OUTPUT: The distance traveled during the time interval.
+
+void distance_in_30_seconds();
+// PURPOSE: Display a chart that breaks down the distance a baseball
+//          traveled in 30 seconds. 
+// INPUT: N/A
+// OUTPUT: An organized chart of the distance that a baseball traveled
+//         in 30 seconds via console.
+
+void time_for_15000m();
+// PURPOSE: Find the time that the baseball needs to travel 15000m.
+// INPUT: N/A
+// OUTPUT: The amount of time in seconds with the resolution of 0.1s.
+
+double distance(int start_time, int end_time){
+    const double G = 9.80665;
+    double distance = 0.5 * G * end_time * end_time - 
+                      0.5 * G * start_time* start_time;
+    return distance;
+}
+
+double distance(double start_time, double end_time){
+    const double G = 9.80665;
+    double distance = 0.5 * G * end_time * end_time - 
+                      0.5 * G * start_time* start_time;
+    return distance;
+}
+
+void distance_in_30_seconds(){
+    const int ARRAY_SIZE = 31, ARRAY_SIZE_2 = 2, DEFAULT_VALUE = 0;
+    int distances[ARRAY_SIZE][ARRAY_SIZE_2] = {{DEFAULT_VALUE, DEFAULT_VALUE}};
+
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(3);
+
+    cout << "Time (in sec):   " << "Distance during interval (in m):   " 
+         << "Total distance (in m):   " << endl; 
+
+}
+
+int main() {
+    cout << distance(1.2, 3.5);
 }
