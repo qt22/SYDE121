6d54ad691a20b049f652d4da028b04751274e004
Jerry Tian <qjtian113@gmail.com>
2020-10-19 22:02:20 -0400

    week 1 exercises done

--- midterm_prep.cpp
+++ midterm_prep.cpp
@@ -6,16 +6,17 @@ void mile_to_km(double distance){
     const double RATE = 1.609347;  
     const int PRECISION = 4;
     double km = distance/RATE;
-    // cout.setf(ios::fixed);
-    // cout.setf(ios::showpoint);
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
     cout.precision(PRECISION);
     cout << "Distance in kilometers: " << km << endl;
 }
 
 // week1_q2
 void polynomial(double x){
-
+    double p = x*x*x + 24*x*x + 12*x + 44;
+    cout << p << endl;
 }
 int main(){
-    mile_to_km(3.2);
+    polynomial(3);
 }
